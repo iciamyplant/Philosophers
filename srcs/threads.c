64b03b8d5af0992c23f6d90dc9d217d38585cbc0
Emmabrdt <emma.bourdit@gmail.com>
2021-07-01 15:57:00 +0200

    number of time each philo must eat ok

--- srcs/threads.c
+++ srcs/threads.c
@@ -6,7 +6,7 @@ void	*is_dead(void	*data)
 
 	ph = (t_philo *)data;
     ft_usleep(ph->pa->die + 1);
-    if ((actual_time() - ph->ms_eat) >= (long)(ph->pa->die))
+    if (ph->pa->stop && ((actual_time() - ph->ms_eat) >= (long)(ph->pa->die)))
     {
         pthread_mutex_lock(&ph->pa->write_mutex);
 		write_status("died\n", ph);
@@ -14,6 +14,8 @@ void	*is_dead(void	*data)
 		ft_usleep(200);
 		//pthread_mutex_unlock(&ph->pa->write_mutex);  
     }
+    if (ph->pa->nb_p_finish == ph->pa->total)
+		ph->pa->stop = 2;
 	return NULL;
 }
 
@@ -23,7 +25,7 @@ void	*thread(void *data)
 
 	ph = (t_philo *)data;
 	if (ph->id % 2 == 0)
-        ft_usleep(ph->pa->eat * 1000); // ?? avec ft_usleep(ph->pa->eat); marchait pas
+        ft_usleep(ph->pa->eat / 10);
 	while (!ph->pa->stop && ph->nb_eat != ph->pa->m_eat)
     {
         pthread_create(&ph->thread_death_id, NULL, is_dead, data);
@@ -32,14 +34,7 @@ void	*thread(void *data)
         pthread_detach(ph->thread_death_id);
     }
     if (ph->nb_eat == ph->pa->m_eat)
-    {
-		pthread_mutex_lock(&ph->pa->write_mutex);
-		ph->pa->nb_p_finish++;
-		write_status("ATE ENOUGH\n", ph);
-		if (ph->pa->nb_p_finish == ph->pa->total)
-			ph->pa->stop = 2;
-		pthread_mutex_unlock(&ph->pa->write_mutex);
-    }
+        ph->pa->nb_p_finish++;
 	return (NULL);
 }
 
